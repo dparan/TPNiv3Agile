cbc1399ba1a58896a80c7a5be7c0c51d16c04d63
Dylan <dylanparant.tel@gmail.com>
2018-11-26 00:19:31 +0100

    TEST

--- tpavion/src/data_model/Passager.java
+++ tpavion/src/data_model/Passager.java
@@ -1,14 +1,22 @@
 package data_model;
 
-public class Passager {
-	
+import java.math.BigInteger;
+
+public class Personne {
+
 	private int id;
-	private String numPasseport;
+	private String prenom;
+	private String nom;
+	private String adresse;
+	private BigInteger noTelephone;
 	
-	Passager(int id,String numPasseport)
-	{
+	public Personne(int id, String prenom, String nom, String adresse, BigInteger noTelephone) {
+		super();
 		this.id = id;
-		this.numPasseport = numPasseport;
+		this.prenom = prenom;
+		this.nom = nom;
+		this.adresse = adresse;
+		this.noTelephone = noTelephone;
 	}
 
 	public int getId() {
@@ -19,12 +27,35 @@ public class Passager {
 		this.id = id;
 	}
 
-	public String getNumPasseport() {
-		return numPasseport;
+	public String getPrenom() {
+		return prenom;
 	}
 
-	public void setNumPasseport(String numPasseport) {
-		this.numPasseport = numPasseport;
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
 	}
 
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getAdresse() {
+		return adresse;
+	}
+
+	public void setAdresse(String adresse) {
+		this.adresse = adresse;
+	}
+
+	public BigInteger getNoTelephone() {
+		return noTelephone;
+	}
+
+	public void setNoTelephone(BigInteger noTelephone) {
+		this.noTelephone = noTelephone;
+	}
 }
